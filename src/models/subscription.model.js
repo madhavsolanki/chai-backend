c67d8259a167ef816a5e94d1d616a1af2e2fca6b
Madhav Solanki <madhav09solanki@gmail.com>
2024-12-05 20:06:08 +0530

    Mage Acoount and User Details Added Successfully

---
+++ src/models/subscription.model.js
@@ -0,0 +1,17 @@
+import mongoose, {Schema} from "mongoose";
+
+const subscriptionSchema = new Schema({
+
+  subscriber:{
+    type:Schema.Types.ObjectId, // One Who is Subscribing
+    ref:"User"
+  },
+
+  channel:{
+    type:Schema.Types.ObjectId,  // One to Whom 'subscriber' is subscribing
+    ref:"User" 
+  }
+
+}, {timestamps:true})
+
+export const Subsciption = mongoose.model("Subsciption", subscriptionSchema);
